bd6c5405ae8e90c0ba6725e023c35080f802788c
Michael Cohen <michaelcohen716@gmail.com>
2019-01-27 22:16:53 -0500

    remove secondary firebae config

--- src/firebase/auth.ts
+++
@@ -1,7 +0,0 @@
-// import firebase from 'firebase'
-// const auth = firebase.auth()
-import { auth } from './firebase'
-
-export const createUser = (email: string, password: string) => {
-  auth.createUserWithEmailAndPassword(email, password)
-}
